be373b754558f8b8a5c7da22e98f2e36d2d1d580
Yasmin Medeiros <yasmin.medeirosapp@gmail.com>
2019-12-29 13:05:42 -0300

    Corrigindo bug da classe Terreno

--- Terreno.cpp
+++ Terreno.cpp
@@ -1,15 +1,32 @@
 #include "Terreno.h"
-#include <iomanip>
 #include <sstream>
-Terreno::Terreno(std::string descricao, int tipoOferta, int valor, Endereco endereco, double area){   this->descricao = descricao;
-    this->tipoOferta = tipoOferta;
-    this->valor = valor;
-    this->endereco = endereco;
+#include <iomanip>
+Terreno::Terreno(std::string descricao, int tipoOferta, int valor, Endereco endereco, 
+double area) : Imovel(descricao,tipoOferta,valor,endereco)
+{  
     this->area = area;
 }
 double Terreno::getArea(){
     return this->area;
 }
 std::string Terreno::toString(){
+     std::stringstream string;
+     string << std::setprecision(2) << std::fixed;
+     string << "\tDescricao: " + getDescricao() << "\n";
+     string << "\tTipo de oferta: ";
+     if(getTipoOferta() == 1)
+        string << "venda\n";
+     else
+        string << "aluguel\n"; 
+    string << "\tPreco: " << getValor() << "\n";
+    string << "\tEndereco:\n";
+    string << "\t\tCidade: " + this->endereco.getCidade() << "\n";
+    string << "\t\tBairro: " << this->endereco.getBairro() << "\n";
+    string << "\t\tCEP: " << this->endereco.getCEP() << "\n";
+    string << "\t\tLogradouro: " << this->endereco.getLogradouro() << "\n";
+    string << "\t\tNumero: " << this->endereco.getNumero() << "\n";
+    string << "\tArea: " << getArea() << "\n";
+
+    return string.str();
 
 }
